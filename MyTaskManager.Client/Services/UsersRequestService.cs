1ad1da4cd96a90c0307926895f4283ab7dfcfe34
AlekseyTrofimov <alekcey.trofimov.99@gmail.com>
2026-09-14 16:55:14 +0300

    added methods in UserService on client

--- MyTaskManager.Client/Services/UsersRequestService.cs
+++ MyTaskManager.Client/Services/UsersRequestService.cs
@@ -1,7 +1,9 @@
 ﻿using MyTaskManager.Client.Models;
+using MyTaskManager.Common.Models;
 using Newtonsoft.Json;
 using System.IO;
 using System.Net;
+using System.Net.Http;
 using System.Text;
 
 namespace MyTaskManager.Client.Services
@@ -9,6 +11,7 @@ namespace MyTaskManager.Client.Services
     public class UsersRequestService
     {
         private const string HOST = "http://localhost:5052/api/";
+        private string _userConroller = HOST + "users";
 
         public AuthToken GetToken(string userName, string password)
         {
@@ -20,7 +23,41 @@ namespace MyTaskManager.Client.Services
             return token;
         }
 
-        private string GetDataByUrl(string url, string userName, string password)
+        public HttpStatusCode CreateUser(AuthToken token, UserDto user)
+        {
+            string userJson = JsonConvert.SerializeObject(user);
+            var result = SendDataByUrl(HttpMethod.Post, _userConroller, token, userJson);
+            return result;
+        }
+
+        public List<UserDto> GetAllUsers(AuthToken token)
+        {
+            string response = GetDataByUrl(_userConroller);
+            List<UserDto> users = JsonConvert.DeserializeObject<List<UserDto>>(response);
+            return users;
+        }
+
+        public HttpStatusCode DeleteUser(AuthToken token, int userId)
+        {
+            var result = DeleteDataByUrl(_userConroller + $"/{userId}", token);
+            return result;
+        }
+
+        public HttpStatusCode CreateMultiplyUser(AuthToken token, List<UserDto> users)
+        {
+            string userJson = JsonConvert.SerializeObject(users);
+            var result = SendDataByUrl(HttpMethod.Post, _userConroller + "/all", token, userJson);
+            return result;
+        }
+
+        public HttpStatusCode UpdateUser(AuthToken token, UserDto user)
+        {
+            string userJson = JsonConvert.SerializeObject(user);
+            var result = SendDataByUrl(HttpMethod.Patch, _userConroller + $"/{user.Id}", token, userJson);
+            return result;
+        }
+
+        private string GetDataByUrl(string url, string userName = null, string password = null)
         {
             string result = string.Empty;
             HttpWebRequest request = (HttpWebRequest)HttpWebRequest.Create(url);
@@ -29,15 +66,48 @@ namespace MyTaskManager.Client.Services
             {
                 string encoded = System.Convert.ToBase64String(Encoding.GetEncoding("ISO-8859-1").GetBytes(userName + ":" + password));
                 request.Headers.Add("Authorization", "Basic " + encoded);
-
-                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
-                using (StreamReader reader = new StreamReader(response.GetResponseStream(), Encoding.UTF8))
-                {
-                    string responseStr = reader.ReadToEnd();
-                    result = responseStr;
-                }
             }
+
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            using (StreamReader reader = new StreamReader(response.GetResponseStream(), Encoding.UTF8))
+            {
+                string responseStr = reader.ReadToEnd();
+                result = responseStr;
+            }
+
             return result;
+        }
+
+        private HttpStatusCode SendDataByUrl(HttpMethod method, string url, AuthToken token, string data)
+        {
+            var result = new HttpResponseMessage();
+            var client = new HttpClient();
+
+            client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", token.access_token);
+
+            var content = new StringContent(data, Encoding.UTF8, "application/json");
+
+            if (method == HttpMethod.Post)
+            {
+                result = client.PostAsync(url, content).Result;
+            }
+            if (method == HttpMethod.Patch)
+            {
+                result = client.PatchAsync(url, content).Result;
+            }
+
+            return result.StatusCode;
+        }
+
+        private HttpStatusCode DeleteDataByUrl(string url, AuthToken token)
+        {
+            var result = new HttpResponseMessage();
+            var client = new HttpClient();
+
+            client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", token.access_token);
+            result = client.DeleteAsync(url).Result;
+
+            return result.StatusCode;
         }
     }
 }
