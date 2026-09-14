4271bd345798f6eb9a286e11d8e6753da274e07f
AlekseyTrofimov <alekcey.trofimov.99@gmail.com>
2026-09-14 17:55:37 +0300

    added tests

--- MyTaskManager.Client/Services/UsersRequestService.cs
+++ MyTaskManager.Client/Services/UsersRequestService.cs
@@ -11,13 +11,13 @@ namespace MyTaskManager.Client.Services
     public class UsersRequestService
     {
         private const string HOST = "http://localhost:5052/api/";
-        private string _userConroller = HOST + "users";
+        private string _usersConrollerUrl = HOST + "users";
 
         public AuthToken GetToken(string userName, string password)
         {
             string url = HOST + "account/token";
 
-            var responseStr = GetDataByUrl(url, userName, password);
+            var responseStr = GetDataByUrl(HttpMethod.Post, url, null, userName, password);
             AuthToken token = JsonConvert.DeserializeObject<AuthToken>(responseStr);
 
             return token;
@@ -26,46 +26,50 @@ namespace MyTaskManager.Client.Services
         public HttpStatusCode CreateUser(AuthToken token, UserDto user)
         {
             string userJson = JsonConvert.SerializeObject(user);
-            var result = SendDataByUrl(HttpMethod.Post, _userConroller, token, userJson);
+            var result = SendDataByUrl(HttpMethod.Post, _usersConrollerUrl, token, userJson);
             return result;
         }
 
         public List<UserDto> GetAllUsers(AuthToken token)
         {
-            string response = GetDataByUrl(_userConroller);
+            string response = GetDataByUrl(HttpMethod.Get, _usersConrollerUrl, token);
             List<UserDto> users = JsonConvert.DeserializeObject<List<UserDto>>(response);
             return users;
         }
 
         public HttpStatusCode DeleteUser(AuthToken token, int userId)
         {
-            var result = DeleteDataByUrl(_userConroller + $"/{userId}", token);
+            var result = DeleteDataByUrl(_usersConrollerUrl + $"/{userId}", token);
             return result;
         }
 
         public HttpStatusCode CreateMultiplyUser(AuthToken token, List<UserDto> users)
         {
             string userJson = JsonConvert.SerializeObject(users);
-            var result = SendDataByUrl(HttpMethod.Post, _userConroller + "/all", token, userJson);
+            var result = SendDataByUrl(HttpMethod.Post, _usersConrollerUrl + "/all", token, userJson);
             return result;
         }
 
         public HttpStatusCode UpdateUser(AuthToken token, UserDto user)
         {
             string userJson = JsonConvert.SerializeObject(user);
-            var result = SendDataByUrl(HttpMethod.Patch, _userConroller + $"/{user.Id}", token, userJson);
+            var result = SendDataByUrl(HttpMethod.Patch, _usersConrollerUrl + $"/{user.Id}", token, userJson);
             return result;
         }
 
-        private string GetDataByUrl(string url, string userName = null, string password = null)
+        private string GetDataByUrl(HttpMethod method, string url, AuthToken token, string userName = null, string password = null)
         {
             string result = string.Empty;
             HttpWebRequest request = (HttpWebRequest)HttpWebRequest.Create(url);
-            request.Method = "POST";
+            request.Method = method.Method;
             if (userName != null && password != null)
             {
                 string encoded = System.Convert.ToBase64String(Encoding.GetEncoding("ISO-8859-1").GetBytes(userName + ":" + password));
                 request.Headers.Add("Authorization", "Basic " + encoded);
+            }
+            else if (token != null)
+            {
+                request.Headers.Add("Authorization", "Bearer " + token.access_token);
             }
 
             HttpWebResponse response = (HttpWebResponse)request.GetResponse();
--- MyTaskManager.ClientTests/Services/UsersRequestServiceTests.cs
+++ MyTaskManager.ClientTests/Services/UsersRequestServiceTests.cs
@@ -1,8 +1,10 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using MyTaskManager.Client.Services;
+using MyTaskManager.Common.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -18,6 +20,31 @@ namespace MyTaskManager.Client.Services.Tests
 
             Console.WriteLine(token.access_token);
             Assert.IsNotNull(token.access_token);
+        }
+
+        [TestMethod()]
+        public void CreateUserTest()
+        {
+            var service = new UsersRequestService();
+            var token = service.GetToken("admin@mail.ru", "qwerty@#$tr55ofimov!");
+
+            UserDto userTest = new UserDto("Petr", "Trofimov", "trof@mail.ru", "qwerty", UserStatus.User, "89203636374");
+
+            var result = service.CreateUser(token, userTest);
+
+            Assert.AreEqual(HttpStatusCode.OK, result);
+        }
+
+        [TestMethod()]
+        public void GetAllUsersTest()
+        {
+            var service = new UsersRequestService();
+            var token = service.GetToken("admin@mail.ru", "qwerty@#$tr55ofimov!");
+
+            var result = service.GetAllUsers(token);
+
+            Console.WriteLine(result.Count);
+            Assert.AreNotEqual(Array.Empty<UserDto>(), result.ToArray());
         }
     }
 }
